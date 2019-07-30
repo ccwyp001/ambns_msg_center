abc8372264a697e95c8e9bfe7a770650ffce3f1a
ccwyp001 <359700595@qq.com>
2019-07-30 16:03:31 +0800

    test

--- pen_apple/api/wsdl/sms.py
+++ pen_apple/api/wsdl/sms.py
@@ -8,13 +8,14 @@ from ...extensions import spyne
 from suds.client import Client
 from ...tasks import send_phone_call
 
+CustomUnicode = Unicode.customize(nullable=False, max_occurs=1)
 
 class SmsWebService(spyne.Service):
     __service_url_path__ = '/msg/SmsWebService.asmx'
     __in_protocol__ = Soap11(validator='lxml')
     __out_protocol__ = Soap11()
 
-    @spyne.srpc(Unicode, Unicode, Unicode, Unicode, _returns=Unicode)
+    @spyne.srpc(CustomUnicode, CustomUnicode, CustomUnicode, CustomUnicode, _returns=CustomUnicode)
     def SendMessage(strLsh, strPhone, strNR, strPartID=''):
         sms_url = current_app.config['SMS_SOAP_URL']
         client = Client(sms_url)
