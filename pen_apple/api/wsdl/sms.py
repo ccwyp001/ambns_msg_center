d85009fcd9f1e1989dc4dc54c9b1b6d7b96c16fe
ccwyp001 <359700595@qq.com>
2019-07-30 16:53:51 +0800

    test

--- pen_apple/api/wsdl/sms.py
+++ pen_apple/api/wsdl/sms.py
@@ -2,6 +2,7 @@
 
 from flask import current_app
 from spyne.protocol.soap import Soap11, Soap12
+from spyne.protocol.json import JsonDocument
 from spyne.model.primitive import Unicode, Integer
 from spyne.model.complex import Iterable
 from ...extensions import spyne
@@ -13,7 +14,7 @@ CustomUnicode = Unicode.customize(nullable=False, max_occurs=1)
 class SmsWebService(spyne.Service):
     __service_url_path__ = '/msg/SmsWebService.asmx'
     __in_protocol__ = Soap12()
-    __out_protocol__ = Soap12()
+    __out_protocol__ = JsonDocument()
 
     @spyne.srpc(CustomUnicode, CustomUnicode, CustomUnicode, CustomUnicode, _returns=CustomUnicode)
     def SendMessage(strLsh, strPhone, strNR, strPartID=''):
